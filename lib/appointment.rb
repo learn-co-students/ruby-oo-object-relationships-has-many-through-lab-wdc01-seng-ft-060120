ad2592c19a9735042f4385972f786ca82d8ac45b
estherkang14 <estherkang14@gmail.com>
2020-06-06 16:30:37 -0400

    finished

---
+++ lib/appointment.rb
@@ -0,0 +1,20 @@
+require 'pry'
+require_relative "./patient.rb"
+require_relative "./doctor.rb"
+
+class Appointment
+    attr_accessor :date, :patient, :doctor
+    @@all = [ ] 
+
+    def initialize(date, patient, doctor)
+        @date = date 
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+end 
