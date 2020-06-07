a42455c57353ed894bf4e1869244dd0112bb7772
Tien Phan <tienphan1989@gmail.com>
2020-06-06 21:49:08 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,19 @@
+require 'pry'
+class Appointment
+    attr_accessor :date, :patient, :doctor 
+    #attr_reader :doctor
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+        #binding.pry
+    end
+
+    def self.all
+        @@all
+    end 
+
+end
